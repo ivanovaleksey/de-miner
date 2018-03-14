ff26dd42590e7f359371b7a82109b8f245160062
Aleksey Ivanov <ialexxei@gmail.com>
2018-03-15 01:38:51 +0300

    Open field

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,22 @@ enum CellKind {
     Safe,
 }
 
+#[derive(Debug, PartialEq)]
+enum OpenCellKind {
+    Bomb,
+    Safe(usize),
+}
+
 #[derive(Debug)]
-struct Field(Vec<CellKind>);
+struct Field {
+    size: usize,
+    cells: Vec<CellKind>,
+}
 
 impl Field {
     fn parse(input: &str) -> Field {
-        let n = input.lines().count();
-        let mut field = Vec::with_capacity(n * n);
+        let size = input.lines().count();
+        let mut cells = Vec::with_capacity(size * size);
 
         for line in input.lines() {
             for c in line.trim().split(' ') {
@@ -26,17 +35,54 @@ impl Field {
                     _ => unreachable!(),
                 };
 
-                field.push(cell);
+                cells.push(cell);
             }
         }
 
-        Field(field)
+        Field { size, cells }
+    }
+
+    fn open(&self) -> Vec<OpenCellKind> {
+        let mut cells = Vec::with_capacity(self.size * self.size);
+
+        for (idx, cell) in self.cells.iter().enumerate() {
+            let open_cell = match *cell {
+                CellKind::Bomb => OpenCellKind::Bomb,
+                CellKind::Safe => {
+                    let x = idx % self.size;
+                    let y = idx / self.size;
+                    let adjacent_cells = Cell(x, y).adjacent();
+                    let adjacent_cells = adjacent_cells
+                        .iter()
+                        .filter(|cell| cell.0 < self.size && cell.1 < self.size);
+
+                    let count = adjacent_cells
+                        .filter(|cell| {
+                            let index = cell.1 * self.size + cell.0;
+                            match self.cells.get(index) {
+                                Some(c) => match *c {
+                                    CellKind::Bomb => true,
+                                    CellKind::Safe => false,
+                                },
+                                None => false,
+                            }
+                        })
+                        .count();
+
+                    OpenCellKind::Safe(count)
+                }
+            };
+
+            cells.push(open_cell);
+        }
+
+        cells
     }
 }
 
 impl PartialEq<Vec<CellKind>> for Field {
     fn eq(&self, other: &Vec<CellKind>) -> bool {
-        &self.0 == other
+        &self.cells == other
     }
 }
 
@@ -48,17 +94,17 @@ impl Cell {
         use std::ops::Range;
         use itertools::Itertools;
 
-        let cols = Range {
+        let x = Range {
             start: self.0.checked_sub(1).unwrap_or(0),
             end: self.0 + 2,
         };
-        let rows = Range {
+        let y = Range {
             start: self.1.checked_sub(1).unwrap_or(0),
             end: self.1 + 2,
         };
 
-        let mut cells = cols.cartesian_product(rows)
-            .map(|pair| Cell(pair.0, pair.1))
+        let mut cells = y.cartesian_product(x)
+            .map(|pair| Cell(pair.1, pair.0))
             .collect::<Vec<_>>();
 
         cells.retain(|cell| cell != self);
@@ -73,15 +119,14 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use CellKind::*;
 
     #[test]
     fn parse_input() {
-        let input = r#"
-            X O O
-            O O O
-            X X O
-        "#;
+        use CellKind::*;
+
+        let input = r#"X O O
+                       O O O
+                       X X O"#;
 
         let field = Field::parse(input);
         let expected = vec![Bomb, Safe, Safe, Safe, Safe, Safe, Bomb, Bomb, Safe];
@@ -91,24 +136,61 @@ mod tests {
     #[test]
     fn find_adjacent_cells() {
         let cell = Cell(0, 0);
-        let expected = vec![Cell(0, 1), Cell(1, 0), Cell(1, 1)];
+        let expected = vec![Cell(1, 0), Cell(0, 1), Cell(1, 1)];
         assert_eq!(cell.adjacent(), expected);
 
-        let cell = Cell(0, 1);
-        let expected = vec![Cell(0, 0), Cell(0, 2), Cell(1, 0), Cell(1, 1), Cell(1, 2)];
+        let cell = Cell(1, 0);
+        let expected = vec![Cell(0, 0), Cell(2, 0), Cell(0, 1), Cell(1, 1), Cell(2, 1)];
         assert_eq!(cell.adjacent(), expected);
 
         let cell = Cell(1, 1);
         let expected = vec![
             Cell(0, 0),
-            Cell(0, 1),
-            Cell(0, 2),
             Cell(1, 0),
-            Cell(1, 2),
             Cell(2, 0),
+            Cell(0, 1),
             Cell(2, 1),
+            Cell(0, 2),
+            Cell(1, 2),
             Cell(2, 2),
         ];
         assert_eq!(cell.adjacent(), expected);
     }
+
+    #[test]
+    fn open_field() {
+        use OpenCellKind::*;
+
+        let input = r#"X O O
+                       O O O
+                       X X O"#;
+
+        let field = Field::parse(input);
+        let expected = vec![
+            Bomb,
+            Safe(1),
+            Safe(0),
+            Safe(3),
+            Safe(3),
+            Safe(1),
+            Bomb,
+            Bomb,
+            Safe(1),
+        ];
+        assert_eq!(field.open(), expected);
+    }
+
+    // #[test]
+    // fn main() {
+    //     let input = r#"
+    //         X O O X X X O O
+    //         O O O O X O X X
+    //         X X O X X O O O
+    //         O X O O O X X X
+    //         O O X X X X O X
+    //         X O X X X O X O
+    //         O O O X O X O X
+    //         X O X X O X O X
+    //     "#;
+    // }
 }
