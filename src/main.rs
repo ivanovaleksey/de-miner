ebfd2ec0e7ca6137a7143bb91229868889a4e4fb
Aleksey Ivanov <ialexxei@gmail.com>
2018-03-15 00:24:04 +0300

    Find adjacent cells

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate itertools;
+
 #[derive(Debug, PartialEq)]
 enum CellKind {
     Bomb,
@@ -38,6 +40,32 @@ impl PartialEq<Vec<CellKind>> for Field {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct Cell(usize, usize);
+
+impl Cell {
+    fn adjacent(&self) -> Vec<Cell> {
+        use std::ops::Range;
+        use itertools::Itertools;
+
+        let cols = Range {
+            start: self.0.checked_sub(1).unwrap_or(0),
+            end: self.0 + 2,
+        };
+        let rows = Range {
+            start: self.1.checked_sub(1).unwrap_or(0),
+            end: self.1 + 2,
+        };
+
+        let mut cells = cols.cartesian_product(rows)
+            .map(|pair| Cell(pair.0, pair.1))
+            .collect::<Vec<_>>();
+
+        cells.retain(|cell| cell != self);
+        cells
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -58,5 +86,29 @@ mod tests {
         let field = Field::parse(input);
         let expected = vec![Bomb, Safe, Safe, Safe, Safe, Safe, Bomb, Bomb, Safe];
         assert_eq!(field, expected);
+    }
+
+    #[test]
+    fn find_adjacent_cells() {
+        let cell = Cell(0, 0);
+        let expected = vec![Cell(0, 1), Cell(1, 0), Cell(1, 1)];
+        assert_eq!(cell.adjacent(), expected);
+
+        let cell = Cell(0, 1);
+        let expected = vec![Cell(0, 0), Cell(0, 2), Cell(1, 0), Cell(1, 1), Cell(1, 2)];
+        assert_eq!(cell.adjacent(), expected);
+
+        let cell = Cell(1, 1);
+        let expected = vec![
+            Cell(0, 0),
+            Cell(0, 1),
+            Cell(0, 2),
+            Cell(1, 0),
+            Cell(1, 2),
+            Cell(2, 0),
+            Cell(2, 1),
+            Cell(2, 2),
+        ];
+        assert_eq!(cell.adjacent(), expected);
     }
 }
